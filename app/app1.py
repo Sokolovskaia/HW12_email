07af15f5c2165418e7c56f2b71cd17734f515be7
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-05-06 00:55:33 +0300

    restructuring

--- app/app1.py
+++ app/app1.py
@@ -1,27 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 from app import db
+from app.db import validate_user
 
 DATABASE_URL = 'data/db.sqlite'
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='Marina_secret_key',
+)
 
 
-@app.route('/')
-def index():
-    user_name = db.get_name(DATABASE_URL)
-    return render_template('index.html', user_name=user_name)
+@app.route('/', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        val = validate_user(username, password)
+
+        if val['success']:
+            session.clear()
+            session['id'] = val['id']
+            session['login'] = val['login']
+            session['first_name'] = val['first_name']
+            session['last_name'] = val['last_name']
+
+            return redirect(url_for('inbox', username=session['id']))
+
+        flash(val['error'])
+
+    return render_template('login.html')
 
 
-@app.route('/mail')
-def mail_input():
-    search_email = request.args.get('search_email', '')
-    searchm = db.search_email_user(DATABASE_URL, search_email)
-    return render_template('mail.html', mails=searchm)
-
-
-#     db.remove_note_by_id(DATABASE_URL, id)
-#     return redirect(url_for('index'))
+@app.route('/inbox')
+def inbox():
+    search_email = session['id']
+    inbox_result = db.inbox_for_user(DATABASE_URL, search_email)
+    return render_template('inbox.html', mails=inbox_result)
 
 
 if __name__ == '__main__':
--- app/db.py
+++ app/db.py
@@ -1,5 +1,9 @@
 import sqlite3
 
+from werkzeug.security import check_password_hash
+
+DATABASE_URL = 'data/db.sqlite'
+
 
 def open_db(db_url):
     db = sqlite3.connect(db_url)
@@ -7,71 +11,28 @@ def open_db(db_url):
     return db
 
 
-def get_name(db_url, page=1):
+def validate_user(login, password):
+    with open_db(DATABASE_URL) as db:
+        result = {'success': False}
+        user = db.execute('SELECT * FROM users WHERE email = :login', {'login': login}).fetchone()
+
+        if user is None:
+            result['error'] = 'Пользователь не найден'
+        elif not user['password'] == password:
+            result['error'] = 'Неверный пароль'
+        else:
+            result['id'] = user['id']
+            result['login'] = user['email']
+            result['first_name'] = user['name']
+            result['last_name'] = user['surname']
+            result['success'] = True
+
+        return result
+
+
+def inbox_for_user(db_url, search_email):
     with open_db(db_url) as db:
-        limit = 50
-        offset = limit * (page - 1)
-        user_name = db.cursor().execute(
-            'SELECT id, surname, name, email, password FROM users LIMIT :limit OFFSET :offset',
-            {'limit': limit, 'offset': offset}
-        ).fetchall()
-        return user_name
-
-
-def search_email_user(db_url, search_email):
-    with open_db(db_url) as db:
-        search = db.cursor().execute(
-            'SELECT u.surname, u.email, l.topic, l.letter_date, l.recipient_id FROM letters l, users u WHERE l.recipient_id = :search_email AND l.sender_id = u.id ORDER BY l.letter_id DESC LIMIT 20', {'search_email': search_email}).fetchall()
-        return search
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# search = request.args.get('search')
-#         if search:
-#             products = product_manager.search_by_name(search)
-#         else:
-#             products = product_manager.items
-#             search=''
-
-
-#
-# def search_notes(db_url, name):
-#     pass
-#
-#
-# def add_note(db_url, note):
-#     pass
-#
-#
-# def update_note(db_url, note):
-#     pass
-#
-#
-# def remove_note_by_id(db_url, note_id):
-#     with open_db(db_url) as db:
-#         db.cursor().execute(
-#             'DELETE FROM notes WHERE id = :id',
-#             {'id': note_id}
-#         )
-#         db.commit()
-#
-#
-# def like_note_by_id(db_url, note_id):
-#     pass
-#
-#
-# def dislike_note_by_id(db_url, note_id):
-#     pass
+        result = db.cursor().execute(
+            'SELECT u.surname, u.email, l.topic, l.letter_date, l.recipient_id FROM letters l, users u WHERE l.recipient_id = :search_email AND l.sender_id = u.id ORDER BY l.letter_id DESC LIMIT 20',
+            {'search_email': search_email}).fetchall()
+        return result
