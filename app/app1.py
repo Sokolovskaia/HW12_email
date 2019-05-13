34979256dbf86374b78ad186e67262524e975936
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-05-14 00:57:12 +0300

    created "base.html"

--- app/app1.py
+++ app/app1.py
@@ -43,39 +43,53 @@ def start():
         inbox_result = db.inbox_for_user(search_email)
         count_result = db.count_inbox_for_menu(search_email)
         return render_template('inbox.html', mails=inbox_result, inbox_count=count_result, user_email=user_email,
-                               user_surname=user_surname)
+                               user_surname=user_surname, active_index='inbox')
 
     @app.route('/outbox')
     def outbox():
         search_email = session['id']
+        user_email = session['login']
+        user_surname = session['last_name']
         outbox_result = db.outbox_for_user(search_email)
         count_result = db.count_inbox_for_menu(search_email)
-        return render_template('outbox.html', mails=outbox_result, inbox_count=count_result)
+        return render_template('inbox.html', mails=outbox_result, inbox_count=count_result, user_email=user_email,
+                               user_surname=user_surname, active_index='outbox')
 
     @app.route('/drafts')
     def drafts():
         search_email = session['id']
+        user_email = session['login']
+        user_surname = session['last_name']
         drafts_result = db.drafts_for_user(search_email)
         count_result = db.count_inbox_for_menu(search_email)
-        return render_template('drafts.html', mails=drafts_result, inbox_count=count_result)
+        return render_template('inbox.html', mails=drafts_result, inbox_count=count_result, user_email=user_email,
+                               user_surname=user_surname, active_index='drafts')
 
     @app.route('/basket')
     def basket():
         search_email = session['id']
+        user_email = session['login']
+        user_surname = session['last_name']
         basket_result = db.basket_for_user(search_email)
         count_result = db.count_inbox_for_menu(search_email)
-        return render_template('basket.html', mails=basket_result, inbox_count=count_result)
+        return render_template('inbox.html', mails=basket_result, inbox_count=count_result, user_email=user_email,
+                               user_surname=user_surname, active_index='basket')
 
     @app.route('/letter/<letter_id>')
     def letter(letter_id):
         letter_result = db.full_letter(letter_id)
         search_email = session['id']
+        user_email = session['login']
+        user_surname = session['last_name']
         count_result = db.count_inbox_for_menu(search_email)
-        return render_template('letter.html', mails=letter_result, inbox_count=count_result)
+        return render_template('letter.html', mails=letter_result, inbox_count=count_result, user_email=user_email,
+                               user_surname=user_surname, active_index='letter')
 
     @app.route('/create_letter', methods=('GET', 'POST'))
     def create_letter():
         search_email = session['id']
+        user_email = session['login']
+        user_surname = session['last_name']
         count_result = db.count_inbox_for_menu(search_email)
         if request.method == 'POST':
             recipient = request.form['recipient']
@@ -87,18 +101,20 @@ def start():
 
             return redirect(url_for('inbox'))
 
-        return render_template('new_letter.html', inbox_count=count_result)
+        return render_template('new_letter.html', inbox_count=count_result, user_email=user_email,
+                               user_surname=user_surname,  active_index='create_letter')
 
     @app.route('/statistics')
     def statistics():
         stat1 = db.statistics_who_writes_to_whom()  # кто с кем переписывается
         stat2 = db.most_letters()  # Кто с кем больше всего переписывается (кол-во отправленных/полученных)
         stat3 = db.ignored_users()  # Кто кого игнорирует (не отвечает на письма)
-        stat4 = db.statistics_who_writes_to_whom_by_units()    # Кто с кем переписывается разрезе отделов (unit'ов)
-        stat5 = db.most_letters_by_units()    # Кто с кем переписывается разрезе отделов (unit'ов)
-        stat6 = db.ignored_users_by_units()    # Кто кого игнорирует (не отвечает на письма) разрезе отделов (unit'ов)
+        stat4 = db.statistics_who_writes_to_whom_by_units()  # Кто с кем переписывается разрезе отделов (unit'ов)
+        stat5 = db.most_letters_by_units()  # Кто с кем переписывается разрезе отделов (unit'ов)
+        stat6 = db.ignored_users_by_units()  # Кто кого игнорирует (не отвечает на письма) разрезе отделов (unit'ов)
         return render_template('statistics.html', result_statistics1=stat1, result_statistics2=stat2,
-                               result_statistics3=stat3, result_statistics4=stat4, result_statistics5=stat5, result_statistics6=stat6)
+                               result_statistics3=stat3, result_statistics4=stat4, result_statistics5=stat5,
+                               result_statistics6=stat6)
 
     if os.getenv('APP_ENV') == 'PROD' and os.getenv('PORT'):
         waitress.serve(app, port=os.getenv('PORT'))
